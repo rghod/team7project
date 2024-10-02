9d50649ebd3d2a31a439a92d3c7734f8408d52c1
rghod <raghadsaad.gha@gmail.com>
2024-10-02 15:36:52 +0300

    Initial Commit

---
+++ Hawen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hawen
+//
+//  Created by Raghad  on 29/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hawen/HawenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HawenApp.swift
+//  Hawen
+//
+//  Created by Raghad  on 29/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HawenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
